6554e8b48a31e4fab42e2f07694a71d0e9c4931c
Asif <107271220+Asif2902@users.noreply.github.com>
2024-10-31 11:31:47 +0600

    update src/controllers/transactionController.js

--- src/controllers/transactionController.js
+++ src/controllers/transactionController.js
@@ -1,31 +1,16 @@
 // src/controllers/transactionController.js
-const { fetchContractTransactions, fetchWalletBalance } = require('../api/blockscoutApi');
+const { fetchContractTransactions } = require('../api/blockscoutApi');
 const { NETWORK_EXPLORER_URL } = require('../config/config');
-const { formatValue, getFunctionName, calculateAge } = require('../utils/helpers');
 
 async function getTransactions(req, res) {
-  const { wallet } = req.query;
   const transactions = await fetchContractTransactions();
-  
-  let walletDetails = null;
-  if (wallet) {
-    const walletTransactions = transactions.filter(
-      tx => tx.from.toLowerCase() === wallet.toLowerCase() || tx.to.toLowerCase() === wallet.to.toLowerCase()
-    );
+  const { wallet } = req.query;
 
-    const totalValue = walletTransactions.reduce((acc, tx) => acc + parseFloat(tx.value), 0);
-    const firstTransaction = walletTransactions[walletTransactions.length - 1];
-    const balance = await fetchWalletBalance(wallet);
+  const filteredTransactions = wallet
+    ? transactions.filter(tx => tx.from.toLowerCase() === wallet.toLowerCase() || tx.to.toLowerCase() === wallet.toLowerCase())
+    : transactions;
 
-    walletDetails = {
-      balance: formatValue(balance),
-      totalTransactions: walletTransactions.length,
-      totalValue: formatValue(totalValue),
-      firstTransaction,
-    };
-  }
-
-  res.render('index', { transactions, wallet, walletDetails, NETWORK_EXPLORER_URL });
+  res.render('index', { transactions: filteredTransactions, wallet, NETWORK_EXPLORER_URL });
 }
 
 async function getTransactionDetails(req, res) {
